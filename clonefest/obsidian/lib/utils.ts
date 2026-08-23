3d96ec7e81eed2218ef8023423338aa5d0f5b9d1
OjasKugore <ojasmkugore.cs25@rvce.edu.in>
2026-08-23 10:00:01 +0530

    reorganizing

--- clonefest/obsidian/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Combines Tailwind CSS class names safely with clsx and tailwind-merge.
- */
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}
